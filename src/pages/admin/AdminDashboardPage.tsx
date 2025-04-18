41ea45e34e3bd520efd64b65245ca7bf08ee24fa
LuchoVera <luchito.espinoza11@gmail.com>
2025-04-18 17:48:53 -0400

    feat: admin board

--- src/pages/admin/AdminDashboardPage.tsx
+++ src/pages/admin/AdminDashboardPage.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { useAuth } from '../../AuthContext';
+import React from "react";
+import { useAuth } from "../../AuthContext";
 
 const AdminDashboardPage: React.FC = () => {
-    const { user } = useAuth();
-    return (
-        <div>
-            <h2>Admin Dashboard</h2>
-            <p>Welcome, {user?.name}!</p>
-            <p>This is the main admin area for tenant: {user?.administeredTenantSubdomain ?? user?.administeredTenantId ?? 'N/A'}</p>
-        </div>
-    );
+  const { user } = useAuth();
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      <p>Welcome, {user?.name}!</p>
+      <p>
+        This is the main admin area for tenant:{" "}
+        {user?.administeredTenantSubdomain ??
+          user?.administeredTenantId ??
+          "N/A"}
+      </p>
+    </div>
+  );
 };
 
 export default AdminDashboardPage;
